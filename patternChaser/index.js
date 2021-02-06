81a33e3409764169b3aed20a4711c02feadaa481
HugoChea <hugochea.pro@outlook.com>
2021-02-06 23:15:46 +0100

    optimized algo and added explaination

--- patternChaser/index.js
+++ patternChaser/index.js
@@ -1,14 +1,22 @@
 function PatternChaser(str) {
     var patterns = [];
+    // i = minimal number of char for a pattern
+    // j = maximum number of char for a pattern (reproduced another time, so a pattern is at most, string length / 2)
     var i = 2; j = Math.floor(str.length / 2);
-    while (i <= j) {
-        FindPattern(i++);
+    
+    // search biggest pattern possible of j length
+    while (j >= i && patterns.length == 0) {
+        FindPattern(j--);
     }
 
     function FindPattern(num) {
+        // For every i+num characters < str.length
         for (var i = 0; i < str.length - num; i++) {
+            // test pattern of num characters from i
             var pattern = str.slice(i, i + num);
+            // get last occurence of pattern in string
             var index = str.lastIndexOf(pattern);
+            // if last occurence is found and last occurence is not the same sequence, push pattern to array and stop loop
             if (index != -1 && index != i) {
                 patterns.push(pattern);
                 break;
@@ -16,9 +24,14 @@ function PatternChaser(str) {
         }
     }
 
-    var largestPattern = patterns.sort(function (a, b) { return a.length - b.length }).pop();
+    if (patterns.length != 0){
+        var largestPattern = patterns[0]
+    }
     if (largestPattern)
         return 'yes ' + largestPattern;
     else
         return 'no null';
 }
+
+console.log(PatternChaser("aabejiabkfabed")) //"yes abe"
+console.log(PatternChaser("123224"))    // "no null"
